60d9dbe16cae936deca63368dd17a07f4ba6282e
viraj-070 <virajbhalarao@gmail.com>
2026-04-10 21:36:46 +0530

    added half code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Route from "./routes/Route";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="w-full h-screen ">
+    <div className="w-full h-screen bg-zinc-900">
       <Navbar />
       <Route />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="w-full h-fit px-4 md:px-0 py-9 max-w-7xl mx-auto bg-[#212529] flex flex-col md:flex-row justify-between items-center gap-4">
+      <div className='w-4/10 bg-amber-400'>hay</div>
+      <div className='w-2/10 bg-red-600'>Footer Content</div>
+      <div className='w-4/10 bg-gree'>More Footer Content</div>
+    </div>
+  );
+}
+
+export default Footer
